caa259dc30fdbdaff77f03ca2ed4fd1210eef108
Francesco Farina <rockerg991@gmail.com>
2018-08-28 17:10:43 +0100

    First commit; goiptv module cli

---
+++ go.mod
@@ -0,0 +1,7 @@
+module github.com/indiependente/goiptv-cli
+
+require (
+	github.com/indiependente/goiptv v1.0.1
+	github.com/indiependente/gospinner v0.0.0-20180828154014-b1bab09f4164
+	github.com/sirupsen/logrus v1.0.6
+)
---
+++ main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"time"
+
+	"github.com/indiependente/goiptv"
+	"github.com/indiependente/gospinner"
+	log "github.com/sirupsen/logrus"
+)
+
+func main() {
+	log.SetLevel(log.ErrorLevel)
+	log.SetFormatter(&log.JSONFormatter{})
+
+	start := time.Now()
+	readers := goiptv.ScrapeAll("extinf sky calcio")
+	folderName := "data_" + start.Format("2006-01-02")
+
+	_ = os.Mkdir(folderName, 0755)
+	go spinner.Spin(os.Stdout, 100*time.Millisecond)
+	fmt.Print("  Scraping and generating playlists... ")
+
+	var i int
+	for r := range readers {
+		data, err := ioutil.ReadAll(r)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Could not read from reader: %v", err)
+		}
+		log.WithFields(log.Fields{"bytes": len(data)}).Debug("bytes received")
+		i++
+		ioutil.WriteFile(fmt.Sprintf(folderName+"/iptv%d.m3u", i), data, 0644)
+	}
+	log.WithFields(log.Fields{"seconds": time.Since(start).Seconds()}).Debug("time elapsed")
+}

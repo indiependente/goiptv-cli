5ae2dd49b19b5b3ce8be82ae1aa08238e290caa4
Francesco Farina <rockerg991@gmail.com>
2018-08-31 00:55:52 +0100

    command line flag support

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ require (
 	github.com/gocolly/colly v1.1.0 // indirect
 	github.com/indiependente/goiptv v1.0.2
 	github.com/indiependente/gospinner v0.0.0-20180828154014-b1bab09f4164
+	github.com/jessevdk/go-flags v1.4.0
 	github.com/sirupsen/logrus v1.0.6
 	golang.org/x/sys v0.0.0-20180828065106-d99a578cf41b // indirect
 	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 // indirect
--- main.go
+++ main.go
@@ -8,39 +8,73 @@ import (
 
 	"github.com/indiependente/goiptv"
 	"github.com/indiependente/gospinner"
+	flags "github.com/jessevdk/go-flags"
 	log "github.com/sirupsen/logrus"
 )
 
+var opts struct {
+	Channels []string `short:"c" long:"channel" description:"A list of tv channels" default:"sky calcio"`
+	Debug    bool     `short:"d" long:"debug" description:"Run program with debug information turned on" optional:"yes"`
+}
+
 func main() {
-	log.SetLevel(log.ErrorLevel)
-	log.SetFormatter(&log.JSONFormatter{})
 
-	start := time.Now()
-	readers := goiptv.ScrapeAll("extinf sky calcio")
-	folderName := "data_" + start.Format("2006-01-02")
+	numPlaylists, timeElapsed := scrapeChannels(opts.Channels)
 
-	_ = os.Mkdir(folderName, 0755)
-	go spinner.Spin(os.Stdout, 100*time.Millisecond)
-	fmt.Print("  Scraping and generating playlists... ")
-
-	var i int
-	for r := range readers {
-		data, err := ioutil.ReadAll(r)
-		if err != nil {
-			log.WithFields(log.Fields{"error": err}).Error("Could not read from reader")
-		}
-		log.WithFields(log.Fields{"bytes": len(data)}).Debug("bytes received")
-		i++
-		err = ioutil.WriteFile(fmt.Sprintf(folderName+"/iptv%d.m3u", i), data, 0644)
-		if err != nil {
-			log.WithFields(log.Fields{"error": err}).Error("Could not write file")
-		}
-	}
-
-	log.WithFields(log.Fields{"seconds": time.Since(start).Seconds()}).Debug("time elapsed")
+	log.WithFields(log.Fields{"seconds": timeElapsed}).Debug("time elapsed")
 	plural := ""
-	if i > 1 {
+	if numPlaylists > 1 {
 		plural = "s"
 	}
-	fmt.Printf("\nSuccessfully retrieved %d m3u playlist%s!\n", i, plural)
+	fmt.Printf("\nSuccessfully retrieved %d m3u playlist%s!\n", numPlaylists, plural)
+}
+
+func scrapeChannels(channels []string) (int, float64) {
+	start := time.Now()
+	var i int
+	for _, c := range channels {
+		log.WithFields(log.Fields{"channel": c}).Debug("search")
+		readers := goiptv.ScrapeAll("extinf " + c)
+		folderName := "data_" + start.Format("2006-01-02")
+
+		_ = os.Mkdir(folderName, 0755)
+		go spinner.Spin(os.Stdout, 100*time.Millisecond)
+		fmt.Print("  Scraping and generating playlists... ")
+
+		for r := range readers {
+			data, err := ioutil.ReadAll(r)
+			if err != nil {
+				log.WithFields(log.Fields{"error": err}).Error("Could not read from reader")
+			}
+
+			log.WithFields(log.Fields{"bytes": len(data)}).Debug("bytes received")
+			i++
+
+			err = ioutil.WriteFile(fmt.Sprintf(folderName+"/iptv%d.m3u", i), data, 0644)
+			if err != nil {
+				log.WithFields(log.Fields{"error": err}).Error("Could not write file")
+			}
+		}
+	}
+	return i, time.Since(start).Seconds()
+}
+
+func init() {
+
+	_, err := flags.Parse(&opts)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Could not parse arguments: %v", err)
+		os.Exit(1)
+	}
+	if opts.Debug {
+		fmt.Printf("Debug mode active\n")
+		log.SetLevel(log.DebugLevel)
+	} else {
+		log.SetLevel(log.ErrorLevel)
+	}
+	log.SetFormatter(&log.JSONFormatter{})
+	if opts.Channels == nil {
+		fmt.Printf("No tv channel argument provided. Defaults research to Sky Calcio\n")
+	}
+
 }
